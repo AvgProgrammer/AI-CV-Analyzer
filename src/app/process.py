cea77a749d3bf2d0b7ea7910596d65368ae75a27
Katsaros Lampros <katsaros.lampros01@gmail.com>
2026-03-05 22:14:39 +0200

    dd initial workflow layout for process.py and user.py

---
+++ src/app/process.py
@@ -0,0 +1,11 @@
+def extractText(file):
+    return True
+
+def extractSkills(text:str):
+    skills=[]
+    return skills
+
+def CompareWithJOb():
+    score= 0
+    return score
+
---
+++ src/app/user.py
@@ -0,0 +1,6 @@
+def importCV(path:str):
+    return True
+
+def sentCv(path:str):
+    return True
+
